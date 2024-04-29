53d6b3acca04bd5ffe386c551aff393e027d10d7
Thigos <dev.findviewbyid@gmail.com>
2024-04-29 00:01:13 -0300

    Pesquisa Binária

--- api-rest-v2/src/main/java/com/example/apirestv2/api/controller/BancosAutorizadosController.java
+++ api-rest-v2/src/main/java/com/example/apirestv2/api/controller/BancosAutorizadosController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,5 +48,20 @@ public class BancosAutorizadosController {
         List<BancoAutorizadoListagemDTO> listaOrdenada = bancoAutorizadoService.ordenacaoPorNome(dto);
 
         return ResponseEntity.ok(listaOrdenada);
+    }
+
+    @GetMapping("/pesquisar-nome/{nome}")
+    @ApiResponse(responseCode = "200", description = "Lista de bancos ordenados por nome")
+    public ResponseEntity<BancoAutorizadoListagemDTO> listarOrdenadoPorNome(@PathVariable String nome) {
+        List<BancoAutorizadoExternoDTO> externoDTO = bancoAutorizadoService.fetchAll();
+
+        if (externoDTO == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        List<BancoAutorizadoListagemDTO> dto = BancoAutorizadoMapper.toListagem(externoDTO);
+        BancoAutorizadoListagemDTO pesquisa = bancoAutorizadoService.pesquisarPorNome(dto, nome);
+
+        return ResponseEntity.ok(pesquisa);
     }
 }
