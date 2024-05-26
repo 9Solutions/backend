ab696ab25d085708c0a1df7a22a415ed0e84b361
DanielleMunakata <danielle.munakata@sptech.school>
2024-05-25 22:59:24 -0300

    Criando a classe teste

---
+++ api-rest-v2/src/test/java/com/example/apirestv2/service/categoria/CategoriaServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.apirestv2.service.categoria;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoriaServiceTest {
+
+    @Test
+    void listAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
