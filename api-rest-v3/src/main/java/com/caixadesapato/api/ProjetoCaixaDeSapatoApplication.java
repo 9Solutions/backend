688fc237221f9d741fb76df6f0644d4e591926c4
eduufreire <eduardo.freire@sptech.school>
2024-07-29 22:01:48 -0300

    novo repositório api-v3

---
+++ api-rest-v3/src/main/java/com/caixadesapato/api/ProjetoCaixaDeSapatoApplication.java
@@ -0,0 +1,13 @@
+package com.caixadesapato.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCaixaDeSapatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCaixaDeSapatoApplication.class, args);
+	}
+
+}
---
+++ api-rest-v3/src/test/java/com/caixadesapato/api/ProjetoCaixaDeSapatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caixadesapato.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCaixaDeSapatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
